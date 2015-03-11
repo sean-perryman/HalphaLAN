92cc4663cac738f17a97d78ee6916f24e3e33482
unknown <sperryman@sean-bwc-it.TechAG.AD>
2015-03-11 14:47:53 -0700

    Switched from MS SQL over to MySQL for ease of transition, and since they are running an old server onsite

---
+++ HalphaLAN/DataViewForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MySql.Data.Common;
+
+namespace HalphaLAN
+{
+    public partial class DataViewForm : Form
+    {
+        private Form aboutBox;
+        private Form helpBox;
+        private BindingSource bSource;
+
+        public DataViewForm()
+        {
+            InitializeComponent();
+
+            //About Box
+            aboutBox = new AboutBox1();
+
+            //Help Box
+            helpBox = new HelpForm();
+
+            //Populate dataGridView1
+            bSource = new BindingSource();
+            MySQL_ToDatagridview();
+
+            // Populate field search box and set the default to last name
+            searchFieldComboBox.Items.Add("Patient ID");
+            searchFieldComboBox.Items.Add("First Name");
+            searchFieldComboBox.Items.Add("Last Name");
+            searchFieldComboBox.SelectedIndex = 2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'gseDataSet.patient' table. You can move, or remove it, as needed.            
+            this.patientTableAdapter.Fill(this.gseDataSet.patient);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string searchFieldColumnName;
+
+            //Pull info from ComboBox
+            switch (searchFieldComboBox.Text)
+            {
+                case "Patient ID":
+                    searchFieldColumnName = "patientID";
+                    break;
+                case "First Name":
+                    searchFieldColumnName = "first_name";
+                    break;
+                case "Last Name":
+                    searchFieldColumnName = "last_name";
+                    break;
+                default:
+                    return;
+            }
+            
+            //Apply Search
+            string searchValue = searchField.Text;
+            
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            bSource.Filter = searchFieldColumnName + " like '%" + searchValue + "%'";
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //Remove filter from binding source to clear the search
+            searchField.Text = "";
+            bSource.RemoveFilter();
+        }
+
+        private void aboutHalphaLANToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            aboutBox.Show();
+        }
+
+        private void helpToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            helpBox.Show();
+        }
+
+        private void MySQL_ToDatagridview()
+        {
+            string connString = "server=192.168.1.89;" +
+                                "uid=gse;" +
+                                "pwd=gsepassword;" +
+                                "database=gse_pt;";
+            MySqlConnection conn;
+           
+            try {
+                conn = new MySqlConnection();
+                conn.ConnectionString = connString;
+                conn.Open();
+                
+                MySqlDataAdapter da = new MySqlDataAdapter();
+                string sqlQuery = "SELECT * FROM patient";
+                da.SelectCommand = new MySqlCommand(sqlQuery, conn);
+
+                DataTable table = new DataTable();
+                da.Fill(table);
+
+                bSource.DataSource = table;
+                
+                dataGridView1.DataSource = bSource;
+
+                conn.Close();
+            } catch (MySqlException e) {
+                MessageBox.Show(e.Message);
+            }
+
+           
+        }
+    }
+}
--- HalphaLAN/Properties/Settings.Designer.cs
+++ HalphaLAN/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace HalphaLAN.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,7 @@ namespace HalphaLAN.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=PIMPYDEATHKILL\\SQLEXPRESS;Initial Catalog=gse;Integrated Security=Tru" +
-            "e")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=GSESERVER\\SQLEXPRESS;Initial Catalog=gse;Integrated Security=True")]
         public string gseConnectionString {
             get {
                 return ((string)(this["gseConnectionString"]));
